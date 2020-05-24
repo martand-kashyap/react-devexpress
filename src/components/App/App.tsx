ba54e6b2aa306426b43f46dd8ba09878a33a6f8d
martand-kashyap <kashyapmartand@gmail.com>
2020-05-24 17:13:49 +0530

    initial commit

---
+++ src/components/App/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+import 'devextreme/dist/css/dx.common.css';
+import 'devextreme/dist/css/dx.light.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>DevExtreme Components Demo</h1>
+    </div>
+  );
+}
+
+export default App;
